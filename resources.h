cc09c4433ac10c60cdb712d1c92bed073002aeda
Arno Deceuninck <arno.deceuninck@student.uantwerpen.be>
2021-02-28 21:14:53 +0100

    Easier to compile

--- resources.h
+++ resources.h
@@ -5,7 +5,7 @@
 #ifndef SCHAKEN_RESOURCES_H
 #define SCHAKEN_RESOURCES_H
 
-const char* path="../resources/";  // pad, relatief ten opzichte van de executable
+const char* path="resources/";  // pad, relatief ten opzichte van de executable
                                    // hier bevinden de speelfiguren zich
 
 #endif //SCHAKEN_RESOURCES_H
